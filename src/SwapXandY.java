5a27eb7d64619874c59a7924a1edb846608fcae8
Ryan KekeL <ryankuswantoridwan@gmail.com>
2023-07-02 16:34:36 +0700

    Test commit satu

---
+++ src/SwapXandY.java
@@ -0,0 +1,14 @@
+public class SwapXandY {
+        public static void main(String[] args) {
+        String x = "asu";
+        String y = "tenan";
+        String temp;
+
+        temp = x;
+        x=y;
+        y=temp;
+        
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+    }
+}
